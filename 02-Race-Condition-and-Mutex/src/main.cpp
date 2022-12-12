3209c7d847ffe9a736ca04ed1d30eb373d1344a9
Kaan Akgundogdu <kaanakgundogdu@gmail.com>
2022-12-12 18:29:09 +0300

    Race Condition and Mutex modified.

--- 02-Race-Condition-and-Mutex/src/main.cpp
+++ 02-Race-Condition-and-Mutex/src/main.cpp
@@ -6,17 +6,19 @@
 #include <cmath>
 #include <limits>
 #include <thread>
+#include <mutex>
 #include "ChiliTimer2.h"
 
 constexpr size_t DATA_SIZE = 5000000;
 
-void ProcessDataSet(std::array<int, DATA_SIZE> &set) {
+void ProcessDataSet(std::array<int, DATA_SIZE> &set, int &sum, std::mutex &mtx) {
 
     for (int x : set)
     {
+        std::lock_guard g { mtx };
         constexpr auto limit = (double)std::numeric_limits<int> ::max();
         const auto y = (double)x / limit;
-        set[0] += int(std::sin(std::cos(y)) * limit);
+        sum+= int(std::sin(std::cos(y)) * limit);
     }
 }
 
@@ -27,6 +29,8 @@ int main()
     std::vector<std::array<int, DATA_SIZE>> datasets{ 4 };
     std::vector<std::thread> workers;
     ChiliTimer2 timer;
+    int sum = 0;
+    std::mutex mtx;
 
     for (auto &arr: datasets)
     {
@@ -37,7 +41,7 @@ int main()
         
     for ( auto &set: datasets)
     {
-        workers.push_back(std::thread{ ProcessDataSet, std::ref(set) });
+        workers.push_back(std::thread{ ProcessDataSet, std::ref(set), std::ref(sum), std::ref(mtx)});
         //ProcessDataSet(set);
     }
 
@@ -48,6 +52,8 @@ int main()
     auto time_passed= timer.Peek();
 
     std::cout << "Processing datasets took " << time_passed << " seconds" << std::endl;
+    std::cout << "Sum of the all element is " << sum << std::endl;
+
     return 0;
 }
 
